421ec49ba77236c67bdf4a0a370cc18bc18958e8
Robert Fruchtman <rfruchtose@gmail.com>
2013-06-02 18:04:57 -0700

    New Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notistat::Application.config.secret_token = '42b78465b88ba1e97456753c21692026e2337344e96d58cf6807c7485008d3a5bc8b607fb6af4e1b27de87d65facb22a18f17a1bd18655e4cc4c7a9eac0ea714'
