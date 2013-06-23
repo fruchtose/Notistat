bcdc561f1d2a91299e6770fa735d7844631819c6
Robert Fruchtman <rfruchtose@gmail.com>
2013-06-23 13:48:08 -0700

    Add comments to Notice.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,14 +1,19 @@
+# Redis backed model
 class Notice < Ohm::Model
+  # Naming module is used so Rails can create RESTful routes
   extend ActiveModel::Naming
 
+  # Boolean state: on or off ("true" or "false" when serialized)
   attribute :status
   reference :user, User
   unique :user_id
 
+  # Overwrites the user accessor because the Ohm-defined method is not compatible with AciveRecord
   def user
     User.find_by_id(user_id)
   end
 
+  # Ohm method is aliased so we can use our own setter
   alias_method :old_status=, :status=
   def status=(value)
     value = value_to_status(value)
@@ -24,12 +29,14 @@ class Notice < Ohm::Model
     "#{value_to_s(status)}"
   end
 
+  # Status and user ID must be present
   def validate
     assert_present :status
     assert_present :user_id
   end
 
   private
+    # Converts a status input to a canonical form
     def value_to_status(value)
       case value
       when nil, false, 0, "0", "false"
