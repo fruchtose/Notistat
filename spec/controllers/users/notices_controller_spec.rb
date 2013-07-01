039deffb8bb22caf0c92c642abcf69b8894e503e
Robert Fruchtman <rfruchtose@gmail.com>
2013-06-30 22:40:27 -0700

    Add controller RSpec tests.

--- spec/controllers/users/notices_controller_spec.rb
+++ spec/controllers/users/notices_controller_spec.rb
@@ -3,16 +3,43 @@ require 'spec_helper'
 describe Users::NoticesController do
   before :each do
     @user = FactoryGirl.create :user
-    sign_in @user
   end
 
-  it "should get show" do
-    get :show, user_id: @user
-    assert_response :success
+  context 'when a user is signed out' do
+    it 'should redirect :show to login' do
+      get :show, user_id: @user
+      assert_response :redirect
+    end
+
+    it 'should redirect :new to login' do
+      get :new, user_id: @user
+      assert_response :redirect
+    end
   end
 
-  it "should get new" do
-    get :new, user_id: @user
-    assert_response :success
+  context 'when a user is signed in' do
+    before :each do
+      sign_in @user
+    end
+
+    it "should get :show" do
+      get :show, user_id: @user
+      assert_response :success
+    end
+
+    it "should get :new" do
+      get :new, user_id: @user
+      assert_response :success
+    end
+  end
+  context 'when a user has no notices' do
+    before :each do
+      sign_in @user
+      #@user.notice.delete if @user.notice
+    end
+    it 'should show status as :off' do
+      get :show, format: :json, user_id: @user
+      assert_response :success
+    end
   end
 end
